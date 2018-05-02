92afbd3b421a26878dbaab57e2b87ea23337937a
Sathish Parameshwara <Sathish.Parameshwara@aexp.com>
2018-05-03 00:45:29 +0530

    Added message and email events

--- server/server.js
+++ server/server.js
@@ -12,14 +12,28 @@ var io=socketIO(server);
 io.on('connection',(socket)=>{
     console.log('New user connected');
 
-    socket.on('createMessage',(message)=>{
-        console.log('creating a message',message);
-        io.emit('newMessage', {
-            from:message.from,
-            text:message.text,
-            createdAt:new Date().getTime()
-        })
+
+    socket.emit('newMessage', {
+        from:'Admin',
+        Message:'Welcome to my chat room'
     });
+
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        Message:'New user joined',
+        createdAt: new Date().getTime()
+    });
+
+
+
+    // socket.on('createMessage',(message)=>{
+    //     console.log('creating a message',message);
+    //     io.emit('newMessage', {
+    //         from:message.from,
+    //         text:message.text,
+    //         createdAt:new Date().getTime()
+    //     })
+    // });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
